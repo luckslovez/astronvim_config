3f51dae816a841dc3ebfddf751e0019b9f833677
Lucas <luckslovez@gmail.com>
2023-05-23 08:36:43 +0200

    Default theme and just packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,6 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.tokyonight" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.rust" },
