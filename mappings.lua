24d36b999ae9769f279863394abcc6c81d4c407a
Lucas <luckslovez@gmail.com>
2023-06-08 09:51:42 +0200

    add todo.txt plugin

--- mappings.lua
+++ mappings.lua
@@ -22,6 +22,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>t"] = { "<cmd>ToDoTxtTasksToggle<cr>" },
+    ["<leader>ta"] = { "<cmd>ToDoTxtCapture<cr>" },
   },
   t = {
     -- setting a mapping to false will disable it
---
+++ plugins/todotxt.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "arnarg/todotxt.nvim",
+    requires = { "MunifTanjim/nui.nvim" },
+    lazy = false,
+    config = function()
+      local todo_file = os.getenv "TODO_FILE"
+      if todo_file then
+        require("todotxt-nvim").setup {
+          todo_file = os.getenv "TODO_FILE" or "/tmp/todo.txt",
+          -- Keymap used in sidebar split
+          keymap = {
+            quit = "q",
+            toggle_metadata = "m",
+            delete_task = "dd",
+            complete_task = "<tab>",
+            edit_task = "ee",
+          },
+        }
+      end
+    end,
+  },
+}
