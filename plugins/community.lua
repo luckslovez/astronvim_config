eb8bc33cebffa17c6a8f363aa97922ce0e9a9389
Lucas <luckslovez@gmail.com>
2024-03-05 20:27:51 +0100

    cleanup community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,28 +3,11 @@ return {
   "AstroNvim/astrocommunity",
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-  { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  { import = "astrocommunity.git.git-blame-nvim" },
-  { import = "astrocommunity.note-taking.obsidian-nvim" },
-  {
-    "epwalsh/obsidian.nvim",
-    opts = {
-      daily_notes = {
-        date_format = "%Y.%m.%d",
-      },
-      templates = {
-        subdir = "templates",
-        date_format = "%Y.%m.%d",
-      },
-    },
-  },
-  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.lua" },
